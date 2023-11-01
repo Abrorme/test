08f1a18d4868dd5850e30a827334048849e8c185
Abrorme <dioblusngx@gmail.com>
2023-11-01 20:27:34 +0500

    sdf

--- Misollar.py
+++ Misollar.py
@@ -1,5 +1,13 @@
 
-def Juft(a):
+def Juft(a): 
+    # bu sonlikni aniqlang uchun funksiya
     if a % 2 == 0:
         return True
     return False 
+
+
+b = [2,3,4,20]
+
+resalt = list(map(lambda a: a**3, b))
+
+print(resalt)
